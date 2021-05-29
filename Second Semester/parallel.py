dbad656582c1b9203be85abc72c19b40e72619a1
AlexandrSlobodnyuk <71066660+AlexandrSlobodnyuk@users.noreply.github.com>
2021-05-29 12:51:03 +0300

    Update parallel.py

--- Second Semester/parallel.py
+++ Second Semester/parallel.py
@@ -2,23 +2,23 @@ import random
 import time
 import sys
 import multiprocessing
-def Pi_finding():
+
+
+def Pi_finding(accuracy):
     def make_cords():
         g = random.Random()
         return [g.random(), g.random()]
     good_dots=0
-    accuracy=100000
-    t0 = time.time()
+    #t0 = time.time()
     dots = []
     for i in range(accuracy):
         dots.append(make_cords())
         if dots[-1][0]**2+dots[-1][1]**2<=1:
             good_dots+=1
-    print("Time spent:", time.time() - t0)
-    print(4*good_dots/accuracy)
-Pi_finding()
+    return(good_dots*4/accuracy)
+#print(Pi_finding(1000000))
 def test_all(pool):
-    l = pool.map(Pi_finding(), [10000000] * 50)
+    l = pool.map(Pi_finding, [1000000] * 4)
     return l
 
 
@@ -29,6 +29,3 @@ if __name__ == '__main__':
     print("Time spent:", time.time() - t0)
 else:
     print(__name__)
-
-
-
